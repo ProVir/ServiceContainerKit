9198b50f4ebbe1deb9618abf3314c16f851e3352
Виталий Короткий <vir@provir.ru>
2020-11-27 22:48:31 +0300

    Skeleton Example project

---
+++ Example/Presentation/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Короткий Виталий on 27.11.2020.
+//  Copyright © 2020 ProVir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
