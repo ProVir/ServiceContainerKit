6d8c37c4e06aee3c1851f9626369674e26139a50
Виталий Короткий <vir@provir.ru>
2021-01-03 14:43:23 +0300

    Example: editor screen

--- Example/Presentation/NoteEdit/NoteEditPresenter.swift
+++ Example/Presentation/NoteEdit/NoteEditPresenter.swift
@@ -9,21 +9,103 @@
 import Foundation
 import Combine
 
+enum NoteEditViewModel {
+    enum EditState: Hashable {
+        case new
+        case newInvalid
+        case changed
+        case invalid
+        case saved
+    }
+}
+
 protocol NoteEditPresenter: class {
+    func configure(
+        showAlertHandler: @escaping (String, String) -> Void
+    )
+    
     var titlePublisher: AnyPublisher<String, Never> { get }
+    var editStatePublisher: AnyPublisher<NoteEditViewModel.EditState, Never> { get }
+    
+    var contentText: String { get set }
+    func save(completion: ((Bool) -> Void)?)
 }
 
 final class NoteEditPresenterImpl: NoteEditPresenter {
     private let editService: NoteRecordEditService
     
+    private var showAlert: (String, String) -> Void = { _, _ in }
+    
+    @Observable
+    var contentText: String
+    
+    var editStatePublisher: AnyPublisher<NoteEditViewModel.EditState, Never> {
+        editService.recordPublisher.combineLatest($contentText).map { savedRecord, currentText in
+            if let savedText = savedRecord?.content.content {
+                if savedText == currentText {
+                    return .saved
+                } else {
+                    return currentText.isEmpty ? .invalid : .changed
+                }
+            } else {
+                return currentText.isEmpty ? .newInvalid : .new
+            }
+        }.removeDuplicates().eraseToAnyPublisher()
+    }
+    
     var titlePublisher: AnyPublisher<String, Never> {
-        editService.recordPublisher.map {
-            $0 != nil ? "Edit note" : "Add note"
-        }.eraseToAnyPublisher()
+        editStatePublisher.map {
+            switch $0 {
+            case .new, .newInvalid: return "New note"
+            case .changed, .saved, .invalid: return "Edit note"
+            }
+        }.removeDuplicates().eraseToAnyPublisher()
     }
     
     init(editService: NoteRecordEditService) {
         self.editService = editService
+        self.contentText = editService.record?.content.content ?? ""
     }
     
+    func configure(
+        showAlertHandler: @escaping (String, String) -> Void
+    ) {
+        self.showAlert = showAlertHandler
+    }
+    
+    func save(completion: ((Bool) -> Void)?) {
+        let title = makeTitle(for: contentText)
+        let content = NoteRecord.Content(title: title, content: contentText)
+        
+        editService.apply(content: content) { [weak self] result in
+            switch result {
+            case .success: completion?(true)
+            case .failure(let error):
+                self?.showErrorAlert(title: "Failed to save note", error: error)
+                completion?(false)
+            }
+        }
+    }
+    
+    // MARK: - Private
+    private func showErrorAlert(title: String, error: Error) {
+        let message = error.localizedDescription
+        showAlert(title, message)
+    }
+    
+    private func makeTitle(for content: String) -> String {
+        let firstLine = content.split(separator: "\n").first ?? ""
+        
+        let maxLength = 32
+        if firstLine.count > maxLength {
+            let str = firstLine.prefix(maxLength)
+            if let index = str.lastIndex(of: " ") {
+                return String(str[str.startIndex..<index])
+            } else {
+                return String(str)
+            }
+        } else {
+            return String(firstLine)
+        }
+    }
 }
--- Example/Presentation/NoteEdit/NoteEditViewController.swift
+++ Example/Presentation/NoteEdit/NoteEditViewController.swift
@@ -15,13 +15,112 @@ class NoteEditViewController: UIViewController {
     private var presenter
     
     private var cancellableSet: Set<AnyCancellable> = []
+    
+    @IBOutlet private var closeBarItem: UIBarButtonItem!
+    @IBOutlet private var saveBarItem: UIBarButtonItem!
+    @IBOutlet private weak var textView: UITextView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationController?.presentationController?.delegate = self
+        
+        presenter.configure(
+            showAlertHandler: { [weak self] title, message in
+                let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                alert.addAction(.init(title: "OK", style: .default, handler: nil))
+                self?.present(alert, animated: true, completion: nil)
+            }
+        )
+        
+        textView.textContainer.lineFragmentPadding = 0
+        updateTextViewInsets(keyboardHeight: 0)
+        
+        textView.text = presenter.contentText
+        textView.delegate = self
+        
         presenter.titlePublisher.sink { [weak self] in
             self?.navigationItem.title = $0
         }.store(in: &cancellableSet)
+        
+        presenter.editStatePublisher.sink { [weak self] in
+            self?.updateForEditState($0)
+        }.store(in: &cancellableSet)
+        
+        NotificationCenter.default
+            .publisher(for: UIResponder.keyboardWillChangeFrameNotification)
+            .sink { [weak self] notification in
+                guard let self = self,
+                      let keyboardValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+                let keyboardScreenEndFrame = keyboardValue.cgRectValue
+                let keyboardViewEndFrame = self.view.convert(keyboardScreenEndFrame, from: self.view.window)
+                let keyboardHeight = keyboardViewEndFrame.height - self.view.safeAreaInsets.bottom
+                self.updateTextViewInsets(keyboardHeight: keyboardHeight)
+                self.scrollToCursorTextView()
+            }.store(in: &cancellableSet)
+        
+        NotificationCenter.default
+            .publisher(for: UIResponder.keyboardWillHideNotification)
+            .sink { [weak self] _ in
+                self?.updateTextViewInsets(keyboardHeight: 0)
+            }.store(in: &cancellableSet)
     }
     
+    // MARK: Actions
+    @IBAction private func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction private func save() {
+        presenter.save(completion: nil)
+    }
+    
+    private func confirmClose() {
+        let alert = UIAlertController(title: "Save changes?", message: nil, preferredStyle: .actionSheet)
+        alert.addAction(.init(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(.init(title: "Save", style: .default, handler: { [weak self] _ in
+            self?.presenter.save(completion: { success in
+                if success {
+                    self?.close()
+                }
+            })
+        }))
+        alert.addAction(.init(title: "Not save", style: .destructive, handler: { [weak self] _ in
+            self?.close()
+        }))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // MARK: - Private
+    private func updateForEditState(_ state: NoteEditViewModel.EditState) {
+        closeBarItem?.title = state == .saved ? "Done" : "Cancel"
+        saveBarItem?.title = (state == .new || state == .newInvalid) ? "Add" : "Save"
+        
+        let canSaved = state == .new || state == .changed
+        saveBarItem?.isEnabled = canSaved
+        isModalInPresentation = canSaved
+    }
+
+    private func updateTextViewInsets(keyboardHeight: CGFloat) {
+        let padding: CGFloat = 16
+        textView.contentInset = .init(top: padding, left: padding, bottom: padding + keyboardHeight, right: padding)
+        textView.scrollIndicatorInsets = .init(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+    }
+    
+    private func scrollToCursorTextView() {
+        let selectedRange = textView.selectedRange
+        textView.scrollRangeToVisible(selectedRange)
+    }
+}
+
+extension NoteEditViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        presenter.contentText = textView.text ?? ""
+    }
+}
+
+extension NoteEditViewController: UIAdaptivePresentationControllerDelegate {
+    func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
+        confirmClose()
+    }
 }
